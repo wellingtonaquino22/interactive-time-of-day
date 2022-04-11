a8c556ea2b6b0570cdf1366505577da77c33537d
wellingtonaquino22 <wel201617@gmail.com>
2022-04-11 18:13:25 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-function carregar() {
+window.onload = () => {
+
+    console.log('Olá')
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = ` Agora são ${hora} hora(s)`
+    
+    if (hora =>0 && hora < 12) {
+        //BOM DIA
+        console.log('Entrou dia')
+        img.src = 'manha.png'
+    }else if (hora=>12 && hora <18){
+        //BOA TARDE
+        console.log('entrou TARDE')
+        img.src = 'tarde.png'
+    }else{
+        //BOA NOITE
+        console.log('Entrou noite')
+        img.src = 'noite.png'
+        //img.innerHTML = '<img src="noite.png">'
+        //imagem.innerHTML = '<img src="noite.jpg">' 
+    }
+    }
+
+/*function carregar() {
+    console.log('Olá')
     var msg = document.getElementById('msg')
     var img = document.getElementById('img')
     var data = new Date()
@@ -17,4 +44,4 @@ function carregar() {
         //img.innerHTML = '<img src="noite.png">'
         //imagem.innerHTML = '<img src="noite.jpg">' 
     }
-}
+}*/
